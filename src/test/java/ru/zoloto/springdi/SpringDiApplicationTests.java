b4c8aeaf688663d144ea7bf44afd283d9d700cfe
Artyom Zolotoverkhov <artyomzolotoverkhov@gmail.com>
2020-04-18 12:10:24 +0300

    added sources

---
+++ src/test/java/ru/zoloto/springdi/SpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zoloto.springdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
